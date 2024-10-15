cf938d5e349f469c052cc1ba3bc8cee39eef7b37
“Rahul <rahul.bhatia@insurancedekho.com>
2024-10-15 23:02:10 +0530

    first commit with test case

---
+++ stringCalculator.test.js
@@ -0,0 +1,6 @@
+// Adding 1st test case to return 0 if no numebr specified
+const { add } = require('./stringCalculator');
+
+test('empty string returns 0', () => {
+    expect(add("")).toBe(0);
+});
