c9dc3ff211caeec8182fc7992ba56af5c303451e
“Rahul <rahul.bhatia@insurancedekho.com>
2024-10-15 23:17:23 +0530

    4th commit with custom delimiter

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,9 +1,16 @@
 
 function add(numbers) {
+    if (numbers.startsWith("//")) {
+        const parts = numbers.split("\n");
+        const delimiter = parts[0].slice(2);
+        const nums = parts[1].split(delimiter);
+        return nums.reduce((sum, num) => sum + Number(num), 0);
+    }
     if (numbers === "") {
         return 0;
     }
     return numbers.split(/[\n,]/).reduce((sum, num) => sum + Number(num), 0);
 }
+
 
 module.exports = { add };
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -19,5 +19,10 @@ test('handles new lines between numbers', () => {
     expect(add("1\n2,3")).toBe(6);
 });
 
+test('supports custom delimiters', () => {
+    expect(add("//;\n1;2")).toBe(3);
+});
+
+
 
 
