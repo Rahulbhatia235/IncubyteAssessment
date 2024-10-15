b2c5184ad6dbc39ab48d5b8a4ecefb2724453e20
“Rahul <rahul.bhatia@insurancedekho.com>
2024-10-15 23:10:07 +0530

    2nd commit added 2 test cases to check single number retunr itself and two number return their sum

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -6,6 +6,13 @@ test('empty string returns 0', () => {
 });
 
 
+test('single number returns itself', () => {
+    expect(add("1")).toBe(1);
+});
+
+test('two numbers return their sum', () => {
+    expect(add("1,5")).toBe(6);
+});
 
 
 
